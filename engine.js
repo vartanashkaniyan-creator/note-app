91d578e8c963c21e36a808121fcdd21d2469a311
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-29 22:36:36 +0330

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,21 @@
+let canGoPreview = false;
 
+function buildApp(command) {
+  if (!command || command.trim() === "") {
+    alert("دستور خالیه");
+    return;
+  }
+
+  console.log("Command:", command);
+  canGoPreview = true;
+  alert("اپ ساخته شد");
+}
+
+function goPreview() {
+  if (!canGoPreview) {
+    alert("اول اپ رو بساز");
+    return;
+  }
+
+  window.location.href = "preview.html";
+}
