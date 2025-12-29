6e519c35ac0a3bd06fe713d500af52599a577a00
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-29 23:28:46 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-function generateApp() {
-  const command = document.getElementById("command").value;
-  const code = Engine.build(command);
-
-  localStorage.setItem("generatedApp", code);
-  alert("اپ ساخته شد ✅ حالا پیش‌نمایش بزن");
-}
-
-function previewApp() {
-  const iframe = document.getElementById("previewFrame");
-  const code = localStorage.getItem("generatedApp");
-
-  if (!code) {
-    alert("اول اپ رو بساز");
-    return;
-  }
-
-  iframe.srcdoc = code;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  Router.init();
+  Router.go("home");
+});
