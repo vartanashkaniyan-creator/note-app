e24bc3fc0e3d29ce852e4a04b3cdea947cf3d69f
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-29 22:37:25 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buildBtn = document.getElementById("buildBtn");
+  const previewBtn = document.getElementById("previewBtn");
 
+  buildBtn.addEventListener("click", () => {
+    const command = document.getElementById("command").value;
+    buildApp(command);
+  });
+
+  previewBtn.addEventListener("click", () => {
+    goPreview();
+  });
+});
