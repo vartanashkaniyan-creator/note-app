29c0b7f4299e7bfab8e2a239769714efbc90bb29
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-29 23:35:31 +0330

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,1 +1,10 @@
+// preview.js
+document.addEventListener("DOMContentLoaded", () => {
+  const app = Storage.load("currentApp");
+  if (!app) return;
 
+  document.body.innerHTML = app.ui;
+  const script = document.createElement("script");
+  script.innerHTML = app.logic;
+  document.body.appendChild(script);
+});
