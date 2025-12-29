ff0dbe48937e39d128a5c72d73bedd9115c6febc
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-29 23:30:41 +0330

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,17 @@
+const Router = {
+  current: "home",
 
+  go(page) {
+    this.current = page;
+    UI.render(page);
+    history.pushState({ page }, "", "#" + page);
+  },
+
+  init() {
+    window.onpopstate = (e) => {
+      const page = e.state?.page || "home";
+      this.current = page;
+      UI.render(page);
+    };
+  }
+};
