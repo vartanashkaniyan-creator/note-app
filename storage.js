0eda0e3c0e460a71c0961714946bd09af08b4452
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-29 23:30:04 +0330

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,13 @@
+const Storage = {
+  save(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
 
+  load(key, def = []) {
+    return JSON.parse(localStorage.getItem(key)) || def;
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  }
+};
