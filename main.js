7ca1f90622030ff78dcea89b5e7d807ed5845182
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-30 22:35:06 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-const runBtn = document.querySelector("button:nth-of-type(1)");
-const clearBtn = document.querySelector("button:nth-of-type(2)");
-const titleInput = document.querySelector("input");
-const textArea = document.querySelector("textarea");
+window.onload = () => {
+  ui.init();
 
-runBtn.onclick = () => {
-  if (!textArea.value.trim()) {
-    alert("دستوری وارد نکردی");
-    return;
+  if (location.hash) {
+    loadPage(location.hash.replace("#", ""));
   }
 
-  alert("دستور اجرا شد:\n\n" + textArea.value);
-};
-
-clearBtn.onclick = () => {
-  titleInput.value = "";
-  textArea.value = "";
+  window.onhashchange = () => {
+    loadPage(location.hash.replace("#", ""));
+  };
 };
