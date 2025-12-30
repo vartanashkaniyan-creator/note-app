468bc4798b6e099fb457447fa229d952bf1813f8
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-30 10:47:15 +0330

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,14 +1,5 @@
-const Storage = {
-  save(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-  },
-
-  load(key, defaultValue = null) {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : defaultValue;
-  },
-
-  remove(key) {
-    localStorage.removeItem(key);
-  }
-};
+function saveNote() {
+  const text = document.getElementById('note').value
+  localStorage.setItem('note', text)
+  alert('Saved')
+}
