c3c1689229490d66bd17a535f867d06cd1fd0c4c
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-30 22:32:13 +0330

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,10 +1,5 @@
 const ui = {
-  renderHome() {
-    document.getElementById("app").innerHTML = `
-      <h2>App Generator</h2>
-      <textarea id="commandInput" rows="4" style="width:90%"></textarea>
-      <br><br>
-      <button onclick="generateAppFromInput()">Generate</button>
-    `;
+  init() {
+    loadPage("home");
   }
 };
