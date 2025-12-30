affd95caa7c8d711bd9fd8722fa172b24df71a85
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-30 09:38:42 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  Router.init();
-  Router.go("home");
+Router.register("home", (app) => {
+  app.append(
+    UI.title("App Creator"),
+    UI.button("Calculator", () => Router.go("calc")),
+    UI.button("Notes", () => Router.go("notes"))
+  );
 });
+
+Router.register("notes", (app) => {
+  const input = UI.input("Write note...");
+  const btn = UI.button("Save", () => {
+    Storage.save("note", input.value);
+    alert("Saved");
+  });
+
+  app.append(UI.title("Notes"), input, btn);
+});
+
+Router.register("calc", (app) => {
+  app.append(UI.title("Calculator (next step)"));
+});
+
+Router.go("home");
