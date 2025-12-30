e0a95be6d428da5912b74094c38473d72d2af27b
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-30 10:35:27 +0330

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,21 +1,10 @@
-let calcValue = "";
-
-function calcPress(val) {
-  calcValue += val;
-  document.getElementById("calcDisplay").value = calcValue;
-}
-
-function calcClear() {
-  calcValue = "";
-  document.getElementById("calcDisplay").value = "";
-}
-
-function calcEqual() {
-  try {
-    calcValue = eval(calcValue).toString();
-    document.getElementById("calcDisplay").value = calcValue;
-  } catch {
-    document.getElementById("calcDisplay").value = "Error";
-    calcValue = "";
+const ui = {
+  renderHome() {
+    document.getElementById("app").innerHTML = `
+      <h2>App Generator</h2>
+      <textarea id="commandInput" rows="4" style="width:90%"></textarea>
+      <br><br>
+      <button onclick="generateAppFromInput()">Generate</button>
+    `;
   }
-}
+};
