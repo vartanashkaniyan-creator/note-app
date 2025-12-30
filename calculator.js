98b109d0fb66d9aae8652b36892dd33d77aceb12
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-30 10:27:18 +0330

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,5 @@
-
+function calc() {
+  const a = Number(document.getElementById('a').value);
+  const b = Number(document.getElementById('b').value);
+  document.getElementById('result').innerText = a + b;
+}
